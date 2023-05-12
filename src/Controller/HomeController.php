46383c32bc0ebb7d9fce7c4e16bafc509b5f1b98
Amel Souidi <amel.souidi@edu.univ-paris13.fr>
2023-05-12 20:36:21 +0200

    edit HomeController add index function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,5 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-   
+    #[Route('/', 'home.index', methods: ['GET'])]
+    public function index(): Response {
+        return $this->render('pages/home.html.twig');
+    }
+
+
 }
