ebac48bddfe535bc9b97c4f6374454c4f1789189
Missipssa6 <slimanimissipssa00@gmail.com>
2023-06-04 02:13:06 +0200

    Add Roles:Admin et viewer au formulaire UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,22 +4,51 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('username')
-            ->add('createdAt')
+            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Viewer' => 'ROLE_VIEWER',
+
+                ],
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passe doivent correspondre.',
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
+            ])
+            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+        ;
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return implode(', ', $rolesArray);
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return explode(', ', $rolesString);
+                }
+            ))
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
