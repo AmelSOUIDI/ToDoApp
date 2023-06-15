2e5c55d642fb4b089d81294a60d6cf3774dac6cf
Missipssa6 <slimanimissipssa00@gmail.com>
2023-06-15 18:54:10 +0200

    Add fonctionnalité of sending Email to new viewer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class UserController extends AbstractController
 {
@@ -48,6 +50,7 @@ class UserController extends AbstractController
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
+        
 
         $form->handleRequest($request);
 
@@ -77,10 +80,11 @@ class UserController extends AbstractController
      * @param Request $request
      * @return RedirectResponse|Response
      */
-    public function createAction2(Task $task,Request $request,UserPasswordHasherInterface $encoder)
+    public function createAction2(Task $task,Request $request,UserPasswordHasherInterface $encoder,MailerInterface $mailer)
     {
         $user = new User();
         $form = $this->createForm(ViewerType::class, $user);
+        
 
         $form->handleRequest($request);
 
@@ -96,8 +100,32 @@ class UserController extends AbstractController
             $user->setPassword($hashedPassword);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
+
+            $email = (new Email())
+            ->from('noreply@gmail.com')
+            ->to($user->getEmail())
+            ->subject('Une nouvelle tâche vous a été partagée!')
+            ->html('<p>J\'espère que ce courriel vous trouve bien. Je tenais à vous informer qu\'une tâche importante vient d\'être partagée avec vous. Cette tâche nécessite votre attention et votre contribution.</p>
         
-            $this->addFlash('success', "Le Viewer a bien été ajouté.");
+            <p>Pour accéder à la tâche partagée, veuillez suivre les instructions ci-dessous:</p>
+            
+            <ol>
+                <li>Connectez-vous à votre compte en utilisant l\'adresse e-mail associée : <strong>votre_nom@gmail.com</strong>.</li>
+                <li>Lors de votre première connexion, veuillez utiliser le mot de passe temporaire suivant : <strong>newviewer</strong>. Vous serez ensuite invité(e) à créer un nouveau mot de passe plus sécurisé.</li>
+            </ol>
+            
+            <p>Une fois connecté(e) à votre compte, vous pourrez consulter la tâche partagée et prendre les mesures appropriées en conséquence. Assurez-vous de lire attentivement toutes les informations fournies et de respecter les délais éventuels associés à cette tâche.</p>
+            
+            <p>Si vous avez des questions ou des préoccupations concernant la tâche ou si vous rencontrez des problèmes lors de la connexion, n\'hésitez pas à me contacter. Je suis là pour vous aider à faciliter ce processus et à assurer une exécution fluide de la tâche.</p>
+            
+            <p>Je vous remercie par avance pour votre collaboration et votre diligence. Votre contribution est essentielle pour atteindre nos objectifs communs. J\'attends avec impatience votre implication dans cette tâche.</p>
+            
+            <p>Cordialement,</p>');
+        
+
+            $mailer->send($email);
+        
+            $this->addFlash('success', "Le Viewer a bien été ajouté et un mail de confirmation lui a etait envoyer!.");
             return $this->redirectToRoute('task_choose_viewer', ['id' => $task->getId()]);
 
         }
